b84363b67e904065a25546d6c12a126dc5c9cf4f
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-06-15 12:11:24 +0600

    added component,css and complete the project

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,9 +1,22 @@
-
+import React,{useState} from 'react'
+import data from './data'
+import SingleQuestion from './SingleQuestion'
 function App() {
+  const [question, setQuestion] = useState(data)
   return (
-    <div className="App">
-      <h1>accortion</h1>
+    <main>
+    <div className='container'>
+      <h3>questions and answers about login</h3>
+      <section className='info'>
+        {question.map((ques) => {
+          return (
+            <SingleQuestion key={ques.id} {...ques}></SingleQuestion>
+          );
+        })}
+      </section>
     </div>
+  </main>
+    
   );
 }
 
