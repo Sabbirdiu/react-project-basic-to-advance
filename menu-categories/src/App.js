b5aa5e2fff9e5516c4edb42022572e155883eb5f
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-06-15 13:39:38 +0600

    display the item

--- menu-categories/src/App.js
+++ menu-categories/src/App.js
@@ -1,9 +1,20 @@
-
+import React,{useState} from 'react'
+import Menu from './components/Menu'
+import Categories from './components/Categories'
+import menu from './data'
 function App() {
+  const [menuItems, setMenuItems] = useState(menu)
   return (
-    <div className="App">
-      <h1>Menu categories</h1>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories  />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
   );
 }
 
---
+++ menu-categories/src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <p>categories</p>
+        </div>
+    )
+}
+
+export default Categories
