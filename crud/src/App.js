0d604149b77d307e8ba8903677a7c5980f68a78d
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-06-18 23:04:45 +0600

    added form and render to list

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import List from './components/List'
+import Aleret from './components/Alert'
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = { id: new Date().getTime().toString(), title: name };
+
+    setList([...list, newItem]);
+    setName('');
+
+  }
   return (
-    <div className="App">
-      <h1>crud</h1>
-    </div>
+    <section className="section-center">
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            submit
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List items={list} />
+        <button className='clear-btn' >
+          clear items
+        </button>
+      </div>
+    </section>
   );
 }
 
