3394c120334be2c422968fcfe90deedfb3f19e96
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-06-21 21:16:15 +0600

    added context boilerplate

--- stripe/src/App.js
+++ stripe/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from "react";
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Submenu from './components/Submenu'
 function App() {
   return (
-    <div className="App">
-      <h1>Hello stripe</h1>
-    </div>
+    <>
+      <Hero />
+      <Navbar />
+      <Sidebar />
+      <Submenu />
+    </>
   );
 }
 
